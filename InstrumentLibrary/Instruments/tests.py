40860445fa97bec5b9d3cfa287276844a4a6e3f2
Nadya <neu5ac@mail.ru>
2025-12-05 14:39:08 +0300

    Add: basic test

--- InstrumentLibrary/Instruments/tests.py
+++ InstrumentLibrary/Instruments/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+from Instruments.models import Instrument
+
+
+class GetPageTest(TestCase):
+    fixtures = ['instruments.json', 'categories.json', 'tags.json', 'specs.json']
+
+    def setUp(self):
+        pass
+
+    def test_mainpage(self):
+        path = reverse('home')
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'Instruments/index.html')
+
+
+    def test_redirect_addpage(self):
+        path = reverse('add_page')
+        redirect_url = reverse('users:login') + '?next=' + path
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, redirect_url)
+
+    def test_data_mainpage(self):
+        i = Instrument.published_objects.all().select_related('cat')
+        path = reverse('home')
+        response = self.client.get(path)
+        self.assertQuerySetEqual(response.context_data['instruments'], i[:3])
+
+    def test_paginate_mainpage(self):
+        path = reverse('home')
+        page = 2
+        paginate_by = 3
+        response = self.client.get(path + f'?page={page}')
+        i = Instrument.published_objects.all().select_related('cat')
+        self.assertQuerySetEqual(response.context_data['instruments'], i[(page - 1) * paginate_by: page * paginate_by])
+
+    def test_instrument_post(self):
+        i = Instrument.published_objects.get(pk=12)
+        path = reverse('instrument', args=[i.slug])
+        response = self.client.get(path)
+        self.assertEqual(i.content, response.context_data['instrument'].content)
+
+    def tearDown(self):
+        pass
--- InstrumentLibrary/users/tests.py
+++ InstrumentLibrary/users/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+
+class RegisterUserTestCase(TestCase):
+
+    def setUp(self):
+        self.data = {
+            'username': 'user_1',
+            'email': 'user1@sitewomen.ru',
+            'first_name': 'Sergey',
+            'last_name': 'Balakirev',
+            'password1': '12345678Aa',
+            'password2': '12345678Aa',
+        }
+
+    def test_form_registration_get(self):
+        path = reverse('users:register')
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'users/register.html')
+
+    def test_user_registration_success(self):
+        user_model = get_user_model()
+        path = reverse('users:register')
+        response = self.client.post(path, data=self.data)
+        users = user_model.objects.filter(username=self.data['username'])
+        self.assertRedirects(response, reverse('users:success'))
+        self.assertTrue(users.exists())
+
+    def test_user_registration_password_error(self):
+        self.data['password2'] = password2 = '2'
+        path = reverse('users:register')
+        response = self.client.post(path, data=self.data)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(response, 'Введенные пароли не совпадают')
+
+    def test_user_registration_exists_error(self):
+        user_model = get_user_model()
+        user_model.objects.create(username=self.data['username'])
+
+        path = reverse('users:register')
+        response = self.client.post(path, data=self.data)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(response, 'Такое имя пользователя уже существует')
+
+
+
+    def tearDown(self):
+        pass
